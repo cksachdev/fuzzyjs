3b5b7586b240d10b07432a913a5acf7d87b10006
Saket Singh <saket.singh@tarento.com>
2019-07-03 14:57:37 +0530

    examples commit for fuzzysearch

---
+++ examples/fuzzysearch.config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+ SERVER: 'mongodb://localhost:27017/',
+ DB: 'htmlcontent',
+ COLLECTION: 'lofuzzysearches',
+ QUERY_FIELD: 'Learning outcome',
+ FRAMEWORK: 'ekstep_ncert_k-12',
+ SCHEMAS_DEFAULT: 'loutcomes',
+ EXCEL_FILE_NAME: 'fuzzy-search-LO.xlsx',
+ EXCEL_SHEET_NAME: 'lo-search-results'
+};
+module.exports = CONFIG;
---
+++ examples/fuzzysearch.js
@@ -0,0 +1,79 @@
+var Fuse = require("fuse.js");
+var request = require('request');
+var termsList = require("./frameworkdata.js");
+const XLSX = require("xlsx");
+var MongoClient = require('mongodb').MongoClient;
+const server = 'mongodb://localhost:27017/';
+const FUZZYSEARCH_CONFIG = require('./fuzzysearch.config.js');
+MongoClient.connect(FUZZYSEARCH_CONFIG.SERVER, {
+ useNewUrlParser: true
+}, function(err, db) {
+ if (err) {
+  throw err;
+  db.close();
+ } else {
+
+  var dbo = db.db(FUZZYSEARCH_CONFIG.DB);
+  var queryparam = FUZZYSEARCH_CONFIG.QUERY_FIELD;
+  dbo.collection(FUZZYSEARCH_CONFIG.COLLECTION).find({}, {
+   projection: {
+    'Learning outcome': 1,
+    _id: 0
+   }
+  }).toArray(function(err, result) {
+   if (err) throw err;
+   console.log(result);
+   var result_array = [];
+   (async () => {
+
+    try {
+
+     var options;
+
+     result.forEach(function(element) {
+
+      var options = {
+       shouldSort: true,
+       includeScore: true,
+       threshold: 0.4,
+       location: 0,
+       distance: 100,
+       maxPatternLength: 53,
+       minMatchCharLength: 1,
+       keys: [
+        "name"
+       ]
+      };
+      var fuse = new Fuse(termsList, options);
+      var sheet_query_name = element[queryparam].trim();
+      var search_result = fuse.search(sheet_query_name);
+      var obj_array = {};
+      if (search_result[0] && search_result[0] !== "undefined") {
+       obj_array.XlSheetName = sheet_query_name;
+       obj_array.Framework_name = search_result[0].item.name;
+       obj_array.score = search_result[0].score;
+      } else {
+       obj_array.XlSheetName = sheet_query_name;
+       obj_array.Framework_name = "No Data found in Framework";
+       obj_array.score = "NA";
+      }
+      result_array.push(obj_array);
+     });
+
+     const ws_name = FUZZYSEARCH_CONFIG.EXCEL_SHEET_NAME;
+     var wb = XLSX.utils.book_new(),
+      ws = XLSX.utils.json_to_sheet(result_array);
+     XLSX.utils.book_append_sheet(wb, ws, ws_name);
+     XLSX.writeFile(wb, FUZZYSEARCH_CONFIG.EXCEL_FILE_NAME);
+
+    } catch (error) {
+
+     console.log('Error in the main function => ' + error);
+    }
+   })();
+
+   db.close();
+
+  });
+ }
+});
